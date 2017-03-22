92685269391e61b571490c2bfbf050bac4a4a32e
Afaren <550047450@qq.com>
2017-03-22 17:31:40 +0800

    afaren - add che operation script che.sh

---
+++ che.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+IP=10.0.0.21
+DATA_DIR=/home/afaren/che_data
+OPERATION="$1"
+NAME=tws
+
+docker run -it --rm \
+	-e CHE_HOST=${IP} \
+    -v ${DATA_DIR}:/data \
+	-v /var/run/docker.sock:/var/run/docker.sock  \
+    eclipse/che ${OPERATION}
